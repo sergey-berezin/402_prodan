a34fbcae7182e1423e38972a23abb535b37ac83d
Regerant <145282552+Regerant@users.noreply.github.com>
2023-10-11 03:22:23 +0600

    Add files via upload

--- second part of lab1V1 with changes/TH_ONNX_V1/Program.cs
+++ second part of lab1V1 with changes/TH_ONNX_V1/Program.cs
@@ -1,20 +1,16 @@
 ﻿using BertAnalizator;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Program
 {
     class program
     {
+        static Semaphore semaphore = new Semaphore(1, 1);
         static async Task Main(string[] args)
         {
             if (args.Length == 0)
             {
-                string path = //@"C:\Users\worka\source\repos\TH_ONNX_V1\text 2.txt";
-                    @args[0];
+                string path =  @args[0];
+                //@"C:\Users\worka\source\repos\TH_ONNX_V1\text 2.txt";
                 string text = File.ReadAllText(path);
                 string modelWebSource = "https://storage.yandexcloud.net/dotnet4/bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
                 Console.WriteLine(text);
@@ -22,26 +18,31 @@ namespace Program
                 CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
                 CancellationToken token = cancelTokenSource.Token;
 
-                var createTask = GetSession.Exist_Download_Model(modelWebSource);
-
-                var Berttoker = await createTask;
-
-                string question;
+                var createTask = await Berttokanalizator.Exist_Download_Model(modelWebSource);
+                var Berttoker = createTask;
+                
+                string question; 
+                semaphore.WaitOne();
                 while ((question = Console.ReadLine()) != "")
                 {
                     var answer = Get_Question(Berttoker, text, question, token);
+                    semaphore.Release();
                 }
             }
-            static async Task Get_Question(GetSession getSession, string text, string question, CancellationToken token)
+            async Task Get_Question(Berttokanalizator getSession, string text, string question, CancellationToken token)
             {
                 try
                 {
-                    var answer = await Task.Run(() => getSession.QA_text_Model(text, question, token));
-                    Console.WriteLine(question + " : " + answer);
+                    
+                    var answer = await Task.Run(() => getSession.QA_text_Model(text, question, token)); 
+                    semaphore.WaitOne();
+                    Console.WriteLine(question + " | " + answer);
+                    semaphore.Release();
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
                 {
-                    Console.WriteLine(question + " : " + ex.Message);
+                    Console.WriteLine(question + " | " + ex.Message);
+                    semaphore.Dispose();
                 }
             }
         }
