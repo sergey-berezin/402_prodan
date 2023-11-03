bf642c00a7d612f3889e3c9048b5e4bd918e7d37
Regerant <145282552+Regerant@users.noreply.github.com>
2023-11-03 16:05:10 +0600

    Waiting individual task

---
+++ Task 2 indv/WPF_Bert_indiv/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using BertAnalizator;
+
+namespace WPF_Bert_indiv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //INDIVIDUAL TASK
+        }
+    }
+}
