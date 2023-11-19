e7b504bc5541ca25535bf82a6a7ec56fdf854416
Regerant <145282552+Regerant@users.noreply.github.com>
2023-11-19 15:23:55 +0600

    The initial version of the program.
    Implemented work with text, history and scroll bars. the following were not implemented: cosmetic improvements to the graphical interface, a reporter error and an application crash due to a story or a 4th text change

---
+++ Task 2 indv/BertAnalizator/BertAnalizator.cs
@@ -0,0 +1,139 @@
+﻿using BERTTokenizers;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace BertAnalizator
+{
+    public class Berttokanalizator
+    {
+        private InferenceSession session;
+        //private List<string> dialogHistory;
+        static Semaphore semaphore = new Semaphore(1,1);
+        static bool isDownloaded = false;
+        public Berttokanalizator()
+        {
+            session = session;
+            //dialogHistory = new List<string>();
+
+        }
+        public async Task Exist_Download_Model()
+        {
+            try
+            {
+                string web_source = "https://storage.yandexcloud.net/dotnet4/bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                var model_path = Path.Combine(path, "bert-large-uncased-whole-word-masking-finetuned-squad.onnx");
+
+                string downaloadedModelPath = model_path;
+
+                if (!File.Exists(model_path))
+                {
+                    downaloadedModelPath = await Download_Model(model_path, web_source);
+                }
+
+                else
+                {
+                    isDownloaded = true;
+                }
+                this.session = new InferenceSession(downaloadedModelPath);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static async Task<string> Download_Model(string model_path, string web_source_model)
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                while (!isDownloaded)
+                {
+                    var stream = await httpClient.GetStreamAsync(web_source_model);
+                    var fileStream = new FileStream(model_path, FileMode.CreateNew);
+                    await stream.CopyToAsync(fileStream);
+                    isDownloaded = true;
+                }
+                if (!File.Exists(model_path))
+                {
+                    throw new Exception("cannot download model!");
+                }
+                return model_path;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<string> QA_text_Model(string context_CTX, string question_QTX, CancellationToken token)
+        {
+            var FactoryTask = await Task.Factory.StartNew<string>(_ =>
+            {
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+                var sentence = $"{{\"question\": {question_QTX}, \"context\": \"@CTX\"}}".Replace("@CTX", context_CTX);
+                var tokenizer = new BertUncasedLargeTokenizer();
+                var tokens = tokenizer.Tokenize(sentence);
+                var encoded = tokenizer.Encode(tokens.Count(), sentence);
+               
+                var bertInput = new BertInput()
+                {
+                    InputIds = encoded.Select(t => t.InputIds).ToArray(),
+                    AttentionMask = encoded.Select(t => t.AttentionMask).ToArray(),
+                    TypeIds = encoded.Select(t => t.TokenTypeIds).ToArray(),
+                };
+
+                var input_ids = ConvertToTensor(bertInput.InputIds, bertInput.InputIds.Length);
+                var attention_mask = ConvertToTensor(bertInput.AttentionMask, bertInput.InputIds.Length);
+                var token_type_ids = ConvertToTensor(bertInput.TypeIds, bertInput.InputIds.Length);
+
+                var input = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("input_ids", input_ids),
+                                                        NamedOnnxValue.CreateFromTensor("input_mask", attention_mask),
+                                                        NamedOnnxValue.CreateFromTensor("segment_ids", token_type_ids) };
+                semaphore.WaitOne();
+                var output = session.Run(input);
+                semaphore.Release();
+
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+                List<float> startLogits = ((IEnumerable<float>)output.ToList().First().Value).ToList();
+                List<float> endLogits = ((IEnumerable<float>)output.ToList().Last().Value).ToList();
+
+                var startIndex = startLogits.ToList().IndexOf(startLogits.Max());
+                var endIndex = endLogits.ToList().IndexOf(endLogits.Max());
+
+                var predictedTokens = tokens
+                            .Skip(startIndex)
+                            .Take(endIndex + 1 - startIndex)
+                            .Select(o => tokenizer.IdToToken((int)o.VocabularyIndex))
+                            .ToList();
+
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+
+                return String.Join(" ", predictedTokens);
+                
+
+            }, token, TaskCreationOptions.LongRunning);
+            return FactoryTask;
+        }
+        public static Tensor<long> ConvertToTensor(long[] inputArray, int inputDimension)
+        {
+            Tensor<long> input = new DenseTensor<long>(new[] { 1, inputDimension });
+
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                input[0, i] = inputArray[i];
+            }
+            return input;
+        }
+        public class BertInput
+        {
+            public long[]? InputIds { get; set; }
+            public long[]? AttentionMask { get; set; }
+            public long[]? TypeIds { get; set; }
+        }
+    }
+}
--- Task 2 indv/WPF_Bert_indiv/MainWindow.xaml.cs
+++ Task 2 indv/WPF_Bert_indiv/MainWindow.xaml.cs
@@ -1,17 +1,80 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
 using BertAnalizator;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System;
 
 namespace WPF_Bert_indiv
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    public abstract class Components : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
     public partial class MainWindow : Window
     {
+        private Berttokanalizator bertAnalizator = new Berttokanalizator();
+        private List<string> dialogHistory = new List<string>();
+        private CancellationTokenSource cancellationTokenSource;
+        private string selectedFilePath;
         public MainWindow()
         {
             InitializeComponent();
-            //INDIVIDUAL TASK
+            //error reporter awaiting
+        }
+        private async void LoadTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                selectedFilePath = openFileDialog.FileName;
+                string text = File.ReadAllText(selectedFilePath);
+                TextDisplay.Text = text;
+                dialogHistory.Clear();
+                DialogHistoryItemsControl.ItemsSource = dialogHistory;
+
+                await bertAnalizator.Exist_Download_Model();
+            }
+        }
+        private async void GetAnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            string question = QuestionInput.Text;
+
+            AddToDialogHistory(question);
+
+            try
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+                string answer = await bertAnalizator.QA_text_Model(TextDisplay.Text, question, cancellationTokenSource.Token);
+                AddToDialogHistory(answer);
+            }
+            catch (TaskCanceledException)
+            {
+                AddToDialogHistory("Answer operation cancelled");
+            }
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
+        }
+
+        private void AddToDialogHistory(string item)
+        {
+            dialogHistory.Add(item);
+            DialogHistoryItemsControl.Items.Refresh();
         }
     }
 }
